bbeabf8819c903c080fe3547f80190d4da334a47
NicoleQA <190654771+shontelleQA@users.noreply.github.com>
2025-10-20 09:23:50 -0400

    Add MyAccountSignedInLocators with LEFT_NAV_LOGOUT_BTN selector

---
+++ 03-ecommerce-workflows/automated-tests/src/pages/MyAccountSignedIn.py
@@ -0,0 +1,14 @@
+from src.selenium_extended import SeleniumExtended
+from src.pages.locators.MyAccountSignedInLocators import MyAccountSignedInLocators as L
+
+class MyAccountSignedIn:
+    def __init__(self, driver):
+        self.driver = driver
+        self.sl = SeleniumExtended(driver)
+
+    def verify_user_is_signed_in(self):
+        """
+        Wait for the Logout link in the left nav to become visible.
+        If visible, assume the user is logged in.
+        """
+        self.sl.wait_until_element_is_visible(L.LEFT_NAV_LOGOUT_BTN)
