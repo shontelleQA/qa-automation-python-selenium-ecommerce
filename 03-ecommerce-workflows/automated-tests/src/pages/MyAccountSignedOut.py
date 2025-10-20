f6ea23e7ee83dca040c6aa42e46b3b913a4b9fba
NicoleQA <190654771+shontelleQA@users.noreply.github.com>
2025-10-20 09:01:56 -0400

    ✨ Extend MyAccountSignedOut page object with registration flow - Added REGISTER_EMAIL, REGISTER_PASSWORD, REGISTER_BUTTON locators - Added input_register_email(), input_register_password(), click_register_button()

--- 03-ecommerce-workflows/automated-tests/src/pages/MyAccountSignedOut.py
+++ 03-ecommerce-workflows/automated-tests/src/pages/MyAccountSignedOut.py
@@ -1,16 +1,15 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from .locators.MyAccountSignedOutLocators import MyAccountSignedOutLocators as L
-from src.selenium_extended import SeleniumExtended   # 👈 new import
+from src.selenium_extended import SeleniumExtended
 from src.helpers.config_helpers import get_base_url
 import logging as logger
-
 
 
 class MyAccountSignedOut:
     def __init__(self, driver):
         self.driver = driver
-        self.sl = SeleniumExtended(driver)           # 👈 new line
+        self.sl = SeleniumExtended(driver)
 
     def go_to_my_account(self):
         base_url = get_base_url()
@@ -39,11 +38,8 @@ class MyAccountSignedOut:
     def click_register_button(self):
         self.sl.wait_and_click(L.REGISTER_BUTTON)
 
-
     def wait_until_error_is_displayed(self, expected_error):
-        """
-        Wait until the expected error message text appears on the page.
-        """
+        """Wait until the expected error message text appears on the page."""
         self.sl.wait_until_element_contains_text(
             L.ERRORS_UL,
             expected_error
--- 03-ecommerce-workflows/automated-tests/src/pages/locators/MyAccountSignedOutLocators.py
+++ 03-ecommerce-workflows/automated-tests/src/pages/locators/MyAccountSignedOutLocators.py
@@ -1,11 +1,14 @@
-# src/pages/locators/MyAccountSignedOutLocators.py
 from selenium.webdriver.common.by import By
 
-class MyAccountSignedOutLocators:
-    # Login form elements
-    LOGIN_USERNAME = (By.ID, "username")            # <input id="username">
-    LOGIN_PASSWORD = (By.ID, "password")            # <input id="password">
-    LOGIN_BUTTON  = (By.CSS_SELECTOR, "button[name='login']")  # <button name="login">
 
-    # We'll add an error message locator later when we verify TCID-12
-    # ERROR_MESSAGE = (By.CSS_SELECTOR, "ul.woocommerce-error li")  # (example if using WooCommerce)
+class MyAccountSignedOutLocators:
+    # existing login locators
+    LOGIN_USERNAME = (By.ID, "username")
+    LOGIN_PASSWORD = (By.ID, "password")
+    LOGIN_BUTTON = (By.CSS_SELECTOR, "button[name='login']")
+    ERRORS_UL = (By.CSS_SELECTOR, "ul.woocommerce-error")
+
+    # 🆕 registration locators
+    REGISTER_EMAIL = (By.ID, "reg_email")
+    REGISTER_PASSWORD = (By.ID, "reg_password")
+    REGISTER_BUTTON = (By.CSS_SELECTOR, "button[value='Register']")
