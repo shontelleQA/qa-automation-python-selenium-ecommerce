dad2f0396033a7be9c29fc759a336d26b0984bf1
NicoleQA <190654771+shontelleQA@users.noreply.github.com>
2025-10-21 21:38:52 -0400

    Fix misplaced cart locators: moved from HomePageLocators to CartPageLocators

--- 03-ecommerce-workflows/automated-tests/src/pages/locators/HomePageLocators.py
+++ 03-ecommerce-workflows/automated-tests/src/pages/locators/HomePageLocators.py
@@ -1,5 +1,10 @@
+# HomePageLocators.py
+# -------------------
+# Contains locator definitions for elements that live on the Home page.
+
 from selenium.webdriver.common.by import By
 
 
 class HomePageLocators:
+    # Locator for the first "Add to cart" button on the homepage
     ADD_TO_CART_BUTTON = (By.CSS_SELECTOR, "a.add_to_cart_button")
